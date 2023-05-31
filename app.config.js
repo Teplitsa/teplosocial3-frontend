5fb3d4330f1ed169855dde4edd58d7d498fa39d9
ahaenor <ahaenor@gmail.com>
2023-05-31 16:52:26 +0300

    * app.config.js is returned to the prod mode.

--- app.config.js
+++ app.config.js
@@ -1,15 +1,15 @@
-const HostName = "teplosocial.tep";
-const BaseUrl = `http://${HostName}`;
+const HostName = "kurst.org";
+const BaseUrl = `https://${HostName}`;
 
 const appConfig = {
-  BaseUrl,
-  AjaxUrl: `${BaseUrl}/wp-admin/admin-ajax.php`,
-  LoginUrl: `${BaseUrl}/simsim`,
-  RestApiUrl: `${BaseUrl}/wp-json`,
-  AuthTokenLifeTimeMs: 600,
-  MongoConnection: "mongodb://teplosocial-mongo:27017",
-  NotificationMicroserviceUrl: `ws://${HostName}:5000`,
-  NotificationRestfulServiceUrl: `${BaseUrl}:5000/api/v1/notifications`,
+    BaseUrl,
+    AjaxUrl: `${BaseUrl}/wp-admin/admin-ajax.php`,
+    LoginUrl: `${BaseUrl}/simsim`,
+    RestApiUrl: `${BaseUrl}/wp-json`,
+    AuthTokenLifeTimeMs: 600,
+    MongoConnection: "mongodb://localhost:27017",
+    NotificationMicroserviceUrl: `wss://${HostName}:5000`,
+    NotificationRestfulServiceUrl: `${BaseUrl}:5000/api/v1/notifications`,
 };
 
 module.exports = appConfig;
